fe713a6605c0e51773f54dd0f1e605afebd4a5af
shhhQuiettt <krzysztof.skro@gmail.com>
2023-08-11 17:43:05 +0200

    ELECTRE III finish implementation

--- electre3.py
+++ electre3.py
@@ -25,12 +25,18 @@ class Criterion:
 def get_outranking_relation(
     variants: pd.DataFrame, criterions: list[Criterion]
 ) -> npt.ArrayLike:
-    if set(c.name for c in criterions) not in set(variants.columns):
-        raise ValueError("Not all criterions are in variants table")
+    if not set(c.name for c in criterions).issubset(set(variants.columns)):
+        raise ValueError(
+            f"Not all criterions are in variants table. Criterions: {[c.name for c in  criterions]}, variants: {variants.columns}"
+        )
 
     comprehensive_concordance_index = get_comprahensive_concordance_index(
         variants, criterions
     )
+
+    marginal_discordance_matrices = [
+        marginal_discordance_index(variants, criterion) for criterion in criterions
+    ]
 
     return comprehensive_concordance_index
 
@@ -79,16 +85,18 @@ def get_marginal_preference(
             variant2.loc[criterion.name]
             < variant1.loc[criterion.name] + criterion.indifference_threshold
         ):
-            return 0
+            return 1
         elif (
             variant2.loc[criterion.name]
             > variant1.loc[criterion.name] + criterion.preference_threshold
         ):
-            return 1
+            return 0
         else:
             return (
-                variant2.loc[criterion.name]
-                - (variant1.loc[criterion.name] + criterion.indifference_threshold)
+                (
+                    (variant1.loc[criterion.name] + criterion.preference_threshold)
+                    - variant2.loc[criterion.name]
+                )
             ) / (criterion.preference_threshold - criterion.indifference_threshold)
 
     elif criterion.criterion_type == CriterionType.COST:
@@ -96,17 +104,70 @@ def get_marginal_preference(
             variant2.loc[criterion.name]
             > variant1.loc[criterion.name] - criterion.indifference_threshold
         ):
-            return 0
+            return 1
         elif (
             variant2.loc[criterion.name]
             < variant1.loc[criterion.name] - criterion.preference_threshold
         ):
-            return 1
+            return 0
         else:
             return (
-                (variant1.loc[criterion.name] - criterion.indifference_threshold)
+                (variant1.loc[criterion.name] - criterion.preference_threshold)
                 - variant2.loc[criterion.name]
-            ) / (criterion.preference_threshold - criterion.indifference_threshold)
+            ) / (criterion.indifference_threshold - criterion.preference_threshold)
 
     else:
+        raise ValueError("Unknown criterion type")
+
+
+def marginal_discordance_index(
+    variants: pd.DataFrame, criterion: Criterion
+) -> npt.NDArray:
+    marginal_discordance_index = np.zeros((len(variants), len(variants)))
+    for variant1, variant2 in itertools.product(variants.index, variants.index):
+        marginal_discordance_index[variant1, variant2] = get_discordance(
+            variants.loc[variant1], variants.loc[variant2], criterion
+        )
+
+    return marginal_discordance_index
+
+
+def get_discordance(
+    variant1: pd.Series, variant2: pd.Series, criterion: Criterion
+) -> float:
+    if criterion.criterion_type == CriterionType.GAIN:
+        if (
+            variant2.loc[criterion.name]
+            > variant1.loc[criterion.name] + criterion.veto_threshold
+        ):
+            return 1
+        elif (
+            variant2.loc[criterion.name]
+            < variant1.loc[criterion.name] + criterion.preference_threshold
+        ):
+            return 0
+
+        else:
+            return (
+                variant2.loc[criterion.name]
+                - (variant1.loc[criterion.name] + criterion.preference_threshold)
+            ) / (criterion.veto_threshold - criterion.preference_threshold)
+
+    elif criterion.criterion_type == CriterionType.COST:
+        if (
+            variant2.loc[criterion.name]
+            < variant1.loc[criterion.name] - criterion.veto_threshold
+        ):
+            return 1
+        elif (
+            variant2.loc[criterion.name]
+            > variant1.loc[criterion.name] - criterion.preference_threshold
+        ):
+            return 0
+        else:
+            return (
+                variant2.loc[criterion.name]
+                - (variant1.loc[criterion.name] - criterion.veto_threshold)
+            ) / (criterion.veto_threshold - criterion.preference_threshold)
+
         raise ValueError("Unknown criterion type")
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from test_data import SEE_PREFERENCES, SEE_NAMES
+from test_data import SEE_PREFERENCES, SEE_NAMES, RESEARCH_ENTITIES_OUTRANKING_MATRIX
 from enum import Enum
 from outranking_relation import OutrankingMatrixNumpy
 import ned
 import seo
 import ranked_pairs
 
-
-see_preference_relation = OutrankingMatrixNumpy(
-    SEE_PREFERENCES,
-    labels=SEE_NAMES,
-)
+# see_preference_relation = OutrankingMatrixNumpy(
+#     SEE_PREFERENCES,
+#     labels=SEE_NAMES,
+# )
 
 # print(ned.ned(SEE_NAMES, 4, see_preference_relation))
-print(ranked_pairs.ranked_pairs(SEE_NAMES, 4, see_preference_relation))
+# print(ranked_pairs.ranked_pairs(SEE_NAMES, 4, see_preference_relation))
+print(RESEARCH_ENTITIES_OUTRANKING_MATRIX)
