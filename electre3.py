33cac9704d25bf30bcd8535284246aa678e7384a
shhhQuiettt <krzysztof.skro@gmail.com>
2023-08-11 16:41:33 +0200

    ELECTRE III implementation

---
+++ electre3.py
@@ -0,0 +1,112 @@
+from dataclasses import dataclass
+import itertools
+import math
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from enum import Enum
+
+
+class CriterionType(Enum):
+    COST = 1
+    GAIN = 2
+
+
+@dataclass(kw_only=True)
+class Criterion:
+    name: str
+    criterion_type: CriterionType
+    weight: float
+    indifference_threshold: float
+    preference_threshold: float
+    veto_threshold: float = math.inf
+
+
+def get_outranking_relation(
+    variants: pd.DataFrame, criterions: list[Criterion]
+) -> npt.ArrayLike:
+    if set(c.name for c in criterions) not in set(variants.columns):
+        raise ValueError("Not all criterions are in variants table")
+
+    comprehensive_concordance_index = get_comprahensive_concordance_index(
+        variants, criterions
+    )
+
+    return comprehensive_concordance_index
+
+
+def get_comprahensive_concordance_index(
+    variants: pd.DataFrame, criterions: list[Criterion]
+):
+    comprehensive_concordance_index = np.zeros((len(variants), len(variants)))
+
+    marginal_concordane_matrices = []
+    for criterion in criterions:
+        marginal_concordane_matrices.append(
+            get_marginal_concordance_index(variants, criterion)
+        )
+
+    for criterion, marginal_concordance_index in zip(
+        criterions, marginal_concordane_matrices
+    ):
+        comprehensive_concordance_index += criterion.weight * marginal_concordance_index
+
+    comprehensive_concordance_index /= sum(criterion.weight for criterion in criterions)
+
+    return comprehensive_concordance_index
+
+
+def get_marginal_concordance_index(
+    variants: pd.DataFrame, criterion: Criterion
+) -> npt.NDArray:
+    if criterion.name not in variants.columns:
+        raise ValueError(f"Criterion {criterion.name} is not in variants table")
+
+    marginal_concordance_index = np.zeros((len(variants), len(variants)))
+    for variant1, variant2 in itertools.product(variants.index, variants.index):
+        marginal_concordance_index[variant1, variant2] = get_marginal_preference(
+            variants.loc[variant1], variants.loc[variant2], criterion
+        )
+
+    return marginal_concordance_index
+
+
+def get_marginal_preference(
+    variant1: pd.Series, variant2: pd.Series, criterion: Criterion
+) -> float:
+    if criterion.criterion_type == CriterionType.GAIN:
+        if (
+            variant2.loc[criterion.name]
+            < variant1.loc[criterion.name] + criterion.indifference_threshold
+        ):
+            return 0
+        elif (
+            variant2.loc[criterion.name]
+            > variant1.loc[criterion.name] + criterion.preference_threshold
+        ):
+            return 1
+        else:
+            return (
+                variant2.loc[criterion.name]
+                - (variant1.loc[criterion.name] + criterion.indifference_threshold)
+            ) / (criterion.preference_threshold - criterion.indifference_threshold)
+
+    elif criterion.criterion_type == CriterionType.COST:
+        if (
+            variant2.loc[criterion.name]
+            > variant1.loc[criterion.name] - criterion.indifference_threshold
+        ):
+            return 0
+        elif (
+            variant2.loc[criterion.name]
+            < variant1.loc[criterion.name] - criterion.preference_threshold
+        ):
+            return 1
+        else:
+            return (
+                (variant1.loc[criterion.name] - criterion.indifference_threshold)
+                - variant2.loc[criterion.name]
+            ) / (criterion.preference_threshold - criterion.indifference_threshold)
+
+    else:
+        raise ValueError("Unknown criterion type")
--- test_data.py
+++ test_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from electre3 import Criterion, CriterionType, get_outranking_relation
+
 SEE_NAMES = ["KAM", "KOS", "KRA", "LEG", "LOD", "MIE", "POM", "SLU", "STA", "TAR"]
 
 
@@ -13,3 +16,90 @@ SEE_PREFERENCES = [
     [0.562, 0.330, 0.326, 0.330, 0.330, 0.330, 0.330, 0.131, 0.000, 0.330],
     [0.670, 0.170, 0.316, 0.670, 0.574, 0.530, 0.800, 0.670, 0.670, 0.000],
 ]
+
+RESEARCH_ENTITIES = pd.DataFrame(
+    columns=[
+        "id",
+        "Publications",
+        "Potential",
+        "Commercialization",
+        "Other achievements",
+    ],
+    data=[
+        [2, 45.93, 2, 0, 17],
+        [4, 64.37, 973.93, 2.02, 92],
+        [11, 94.11, 1121, 5.06, 92],
+        [36, 51.88, 235.2, 0.68, 35],
+        [389, 62.12, 210.06, 1.23, 46],
+        [390, 70.25, 194.05, 0.81, 58],
+        [395, 90.54, 242.61, 0.68, 63],
+        [402, 89.75, 1165.43, 4.34, 83],
+        [403, 63.08, 480.46, 6.11, 58],
+        [410, 57.82, 132.9, 0.19, 35],
+        [418, 52.22, 100.72, 0.89, 42],
+        [424, 75.75, 302.5, 2.48, 65],
+        [433, 61.54, 861.18, 2.07, 85],
+        [442, 57.45, 498.39, 1.62, 43],
+        [448, 79.03, 269.77, 7.48, 80],
+        [449, 58.15, 508.26, 1.2, 69],
+        [451, 88.82, 525.47, 5.24, 82],
+        [458, 53.72, 284.18, 0.86, 50],
+        [463, 70.27, 1282.04, 2.12, 71],
+        [466, 60.86, 323, 2.92, 75],
+        [476, 53.66, 203.09, 0.45, 38],
+        [485, 77.09, 1408, 5.69, 90],
+        [486, 63.76, 524.51, 3.02, 41],
+        [503, 80.18, 1141.07, 2.68, 92],
+        [504, 112.37, 330.9, 4.76, 88],
+        [505, 61.11, 395.57, 3.72, 80],
+        [515, 64.93, 473.1, 10.15, 69],
+        [529, 60.29, 11, 0, 0],
+        [825, 62.5, 108.56, 4.68, 40],
+        [897, 65.65, 345.43, 2.34, 76],
+        [904, 27.58, 14, 4.17, 33],
+        [910, 77.24, 678, 13.32, 48],
+        [912, 48.59, 492.97, 4.34, 31],
+        [926, 48, 0, 0, 22],
+        [958, 70.13, 32.43, 4.45, 42],
+        [960, 63.16, 20, 0, 37],
+        [975, 0, 0.2, 0, 17],
+        [987, 60.85, 367.16, 1.47, 71],
+        [988, 55.66, 274.4, 0.38, 65],
+    ],
+)
+
+RESEARCH_ENTITIES_CRITERIA = [
+    Criterion(
+        name="Publications",
+        criterion_type=CriterionType.GAIN,
+        weight=0.65,
+        indifference_threshold=5,
+        preference_threshold=15,
+        veto_threshold=30,
+    ),
+    Criterion(
+        name="Potential",
+        criterion_type=CriterionType.GAIN,
+        weight=0.1,
+        indifference_threshold=20,
+        preference_threshold=50,
+    ),
+    Criterion(
+        name="Commercialization",
+        criterion_type=CriterionType.GAIN,
+        weight=0.15,
+        indifference_threshold=0.5,
+        preference_threshold=1.2,
+    ),
+    Criterion(
+        name="Other achievements",
+        criterion_type=CriterionType.GAIN,
+        weight=0.1,
+        indifference_threshold=4,
+        preference_threshold=12,
+    ),
+]
+
+RESEARCH_ENTITIES_OUTRANKING_MATRIX = get_outranking_relation(
+    RESEARCH_ENTITIES, RESEARCH_ENTITIES_CRITERIA
+)
